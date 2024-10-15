383b02cc94b759a4e53c51575ea4f46bcc10bff1
t0rnado11 <82468908+t0rnado11@users.noreply.github.com>
2024-10-15 23:07:26 +0100

    Update index.js
    Added testing function

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const createBot = require("./bot/bot.js")
+const hash = require("./util/hash.js")
 
+createBot(hash.genHash(), {
+  host: "kaboom.pw",
+  username: "testbot_"+hash.randomstring(5),
+  version: "1.20.1"
+})
