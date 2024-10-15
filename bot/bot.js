e3d6fb4cc08227a92a98039cd583e2ad763ea9e4
t0rnado11 <82468908+t0rnado11@users.noreply.github.com>
2024-10-15 22:29:50 +0100

    Update bot.js
    add basic functions

--- bot/bot.js
+++ bot/bot.js
@@ -1,1 +1,9 @@
+const mineflayer = require("mineflayer")
+const Vec3 = require("vec3")
 
+module.exports = function createBot(args) {
+  var bot = mineflayer.createBot(args)
+  bot.onTick = []
+  bot.autoRejoin = true
+  
+}
