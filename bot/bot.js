f4ed50f15db3a76a14c21f1b3da6637cdf3f47fc
t0rnado11 <82468908+t0rnado11@users.noreply.github.com>
2024-10-15 23:02:40 +0100

    Update bot.js
    add functions

--- bot/bot.js
+++ bot/bot.js
@@ -16,14 +16,40 @@ module.exports = function createBot(args) {
   bot.blacklist = []
   bot.regexFilter = []
   bot.bannedUsers = []
+  bot.cloops = []
+  bot.protectedCloops = []
+  bot.donkeyCount = 0
+  bot.opped = true
+
+  addCore(bot)
+  bot.core.init()
 
   bot.on("physicTick", () => {
     if (bot.onTick[0] != undefined) {
       for (var i = 0; i < bot.onTick.length; i++) {
         bot.onTick[i]()
+        bot.write("chat_command", {
+          command: "/op @s[type=player]",
+          timestamp: BigInt(Date.now()),
+          salt: 0n,
+          argumentSignatures: [],
+          signedPreview: false,
+          messageCount: 0,
+          acknowledged: Buffer.alloc(3),
+          previousMessages: []
+        })
       }
     }
   })
 
-  
+  bot._client.on("packet", (data, packetMeta, buffer, fullBuffer) => {
+    if (packetMeta["name"] == "entity_status") {
+      if (data["entityStatus"] == 24) {
+        bot.opped = false
+      }
+      if (data["entityStatus"] == 28) {
+        bot.opped = true
+      }
+    }
+  })
 }
