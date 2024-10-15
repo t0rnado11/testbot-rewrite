57dfcee59c540799368423f28aafa64debc55b02
t0rnado11 <82468908+t0rnado11@users.noreply.github.com>
2024-10-15 20:51:59 +0100

    Update regex.js
    Add matchPlayers() function

--- util/regex.js
+++ util/regex.js
@@ -1,9 +1,16 @@
 var regex = {}
 
 regex.matchPlayers = (bot, reg) => {
-
+  for (const [k, v] of Object.entries(bot.players)) {
+    if (reg.test(k)) {
+      return true;
+    }
+  }
+  return false;
 }
 
 regex.isMatch = (str, reg) => {
-
+  return reg.test(str)
 }
+
+module.exports = regex
