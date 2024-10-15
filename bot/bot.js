6a32fc0d98ff12b0c411a71a8e6749454ab1a666
t0rnado11 <82468908+t0rnado11@users.noreply.github.com>
2024-10-15 22:52:47 +0100

    Update bot.js
    Add bot variables

--- bot/bot.js
+++ bot/bot.js
@@ -5,5 +5,22 @@ module.exports = function createBot(args) {
   var bot = mineflayer.createBot(args)
   bot.onTick = []
   bot.autoRejoin = true
+  bot.ownerAuthed = []
+  bot.trustAuthed = []
+  bot.ownerHash = ""
+  bot.prefix = "+"
+  bot.globalPrefix = "testbot:"
+  bot.blacklist = []
+  bot.regexFilter = []
+  bot.bannedUsers = []
+
+  bot.on("physicTick", () => {
+    if (bot.onTick[0] != undefined) {
+      for (var i = 0; i < bot.onTick.length; i++) {
+        bot.onTick[i]()
+      }
+    }
+  })
+
   
 }
