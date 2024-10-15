485788b23f0fac34d2a03d55f4837d679154a592
t0rnado11 <82468908+t0rnado11@users.noreply.github.com>
2024-10-15 20:45:47 +0100

    Update command.js
    Add Command.new function

--- command/command.js
+++ command/command.js
@@ -1,1 +1,14 @@
+// this module doesn't serve much purpose other than making code look nice
 
+var Command = {}
+
+Command.new = (name, aliases, perm, callback) => {
+  return {
+    name: name,
+    aliases: aliases,
+    permission: perm,
+    func: callback
+  }
+}
+
+module.exports = Command
